30fa429594b40585e72c2df70533f754ab94ef98
gustiaryapriandana31 <gustiaryapriandana@gmail.com>
2024-05-20 22:52:15 +0700

    feat:create other products page

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 // // Parallax Effect
-// const buttonFindYours = document.getElementById("find_yours");
-// const textWatch = document.querySelector(".text-watch");
-// const textErs = document.querySelector(".text-ers");
+const buttonFindYours = document.getElementById("find_yours");
+const textWatch = document.querySelector(".text-watch");
+const textErs = document.querySelector(".text-ers");
 
-// window.onscroll = () => {
-//   let y = window.scrollY;
-//   console.log(y);
+window.onscroll = () => {
+  let y = window.scrollY;
+  console.log(y);
 
-//   buttonFindYours.style.transform = `translateX(-${y * 2.5}px)`;
-//   textWatch.style.transform = `translateX(-${y * 1.5}px)`;
-//   textErs.style.transform = `translateX(${y * 1.5}px)`;
-// };
+  buttonFindYours.style.transform = `translateX(-${y * 2.5}px)`;
+  textWatch.style.transform = `translateX(-${y * 1.5}px)`;
+  textErs.style.transform = `translateX(${y * 1.5}px)`;
+};
 
-// // Mobile Navigation
-// const menu = document.getElementById("menu");
-// const navMobile = document.getElementById("nav-mobile");
+// Mobile Navigation
+const menu = document.getElementById("menu");
+const navMobile = document.getElementById("nav-mobile");
 
-// menu.addEventListener("click", () => {
-//   navMobile.classList.toggle("animated");
+menu.addEventListener("click", () => {
+  navMobile.classList.toggle("animated");
 
-//   if (navMobile.classList.contains("hide")) {
-//     navMobile.classList.remove("hide");
-//     navMobile.classList.add("show");
-//   } else {
-//     navMobile.classList.remove("show");
-//     navMobile.classList.add("hide");
-//   }
-// });
+  if (navMobile.classList.contains("hide")) {
+    navMobile.classList.remove("hide");
+    navMobile.classList.add("show");
+  } else {
+    navMobile.classList.remove("show");
+    navMobile.classList.add("hide");
+  }
+});
 
 // Watch Contents Tabbed Navigation
 let buttons = document.querySelectorAll(".button");
@@ -87,3 +87,12 @@ closeButton.forEach(function (closeB) {
     });
   });
 });
+
+
+const mobileLink = document.querySelectorAll(".link");
+mobileLink.forEach(function (link){
+  link.addEventListener("click", () => {
+    navMobile.classList.remove("active");
+    navMobile.classList.add("active");
+  });
+}) 
